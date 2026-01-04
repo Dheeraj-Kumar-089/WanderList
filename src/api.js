579a4667867139c956bb57f0aca08dc105a5b7f5
Dheeraj <dheerajkp810@gmail.com>
2026-01-05 01:47:06 +0530

    Update API URLs for production2

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  // Render par deploy karne ke baad jo URL mila, use yahan dalein
+
   baseURL: "https://wanderlist-77wx.onrender.com/api/listings", 
-  withCredentials: true, // Cookies (Passport sessions) ke liye ye sabse zaroori hai
+  withCredentials: true, 
 });
 
 export default API;
