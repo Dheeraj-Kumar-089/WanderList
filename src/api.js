bf764bd8d06a622973ac538d5652556fe9a203d5
Dheeraj <dheerajkp810@gmail.com>
2026-01-05 01:55:48 +0530

    Update API URLs for production3

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "https://wanderlist-77wx.onrender.com/api/listings", 
+  baseURL: "https://wanderlist-77wx.onrender.com/api", 
   withCredentials: true, 
 });
 
